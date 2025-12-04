4c7a4b2a5b34754c66b9c9b1d5b74d9246b5dc07
Vishva <vishwa.muddasani@serole.com>
2025-12-04 11:16:11 +1100

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -20,6 +20,69 @@ app.post('/signup', async (req, res) => {
     }
 });
 
+
+//Get a single user by email
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.email;
+    const user = await User.findOne({ email: userEmail });
+    try {
+        if(!user) {
+        return res.status(404).send("User not found");
+        }
+        else {
+            res.send(user);
+        }
+    } catch(err) {
+        res.status(400).send("Error fetching user" + err.message);
+    }
+});
+
+//Feed API - GET /feed - get all the users fromn the database
+app.get('/feed', async (req, res) => {
+    const users = await User.find();
+    try {
+        if(users.length === 0) {
+        return res.status(404).send("Users not found");
+        }
+        else {
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(400).send("Error fetching user" + err.message);
+    }
+});
+
+//Delete API - DELETE /user - delete a user by id
+app.delete('/user', async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        await User.findByIdAndDelete({ _id: userId });
+        res.send("User deleted successfully");
+    } catch(err) {
+        res.status(400).send("Error fetching user" + err.message);
+    }
+});
+
+//Update API - PATCH /user - Update data of the user
+app.patch('/user/:userId', async (req, res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
+    try {
+            const ALLOWED_UPDATES = ["photoUrl", "about", "skills", "age", "password"];
+            const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+            if(!isUpdateAllowed) {
+                throw new Error("Invalid updates!");
+            }
+            const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: 'before',
+                runValidators: true
+            });
+            console.log(user);
+            res.send("User updated successfully");
+    } catch(err) {
+        res.status(400).send("Error fetching user" + err.message);
+    }
+});
+
 connectDB()
 .then(() => {
     console.log("Connected to MongoDB successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,47 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50
     },
     lastName: {
         type: String,
     },
     email: {
         type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        min: 18,
     },
     gender: {
         type: String,
+        validate(value) {
+            if(!["male", "female", "others"].includes(value.toLowerCase())) {
+                throw new Error("Invalid gender value");
+            }
+        }
+    },
+    photoUrl: {
+        type: String
+    },
+    about: {
+        type: String,
+        default: "This is the default description about the user."
+    },
+    skills: {
+        type: [String],
     }
-});
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 
