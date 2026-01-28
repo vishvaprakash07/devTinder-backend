e1db892f8e30551e8e2b7a11fdd6997bf07ece55
Vishva <vishvaprakash07@gmail.com>
2026-01-28 22:57:34 +1100

    updated cors options and login response

--- src/app.js
+++ src/app.js
@@ -10,7 +10,12 @@ const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 
 
-app.use(cors);
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -53,7 +53,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000), // cookie will be removed after 8 hours
       });
-      res.send("Login successful");
+      res.send(user);
     } else {
       throw new Error("Invalid credentials");
     }
