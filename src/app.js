884bf67c5fae38e699c4f8ca49a345d96f238648
Vishva <vishvaprakash07@gmail.com>
2026-01-28 22:24:45 +1100

    installed cors package

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 const authRouter = require('./routes/auth');
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request"); 
 const userRouter = require("./routes/user");
 
-const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(cookieParser());
 
